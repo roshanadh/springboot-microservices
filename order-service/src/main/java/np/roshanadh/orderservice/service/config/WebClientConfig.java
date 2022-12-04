cb7dac5abbe4fa97a3635808822a65b1c0b2aa5a
Roshan Adhikari <nahsorad@gmail.com>
2022-12-05 02:51:44 +0545

    Integrate WebClient

---
+++ order-service/src/main/java/np/roshanadh/orderservice/service/config/WebClientConfig.java
@@ -0,0 +1,14 @@
+package np.roshanadh.orderservice.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+  @Bean
+  public WebClient webClient() {
+    return WebClient.builder().build();
+  }
+}
