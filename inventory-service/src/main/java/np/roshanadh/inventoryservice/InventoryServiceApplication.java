9cecb714455e649f638a31b968dc16258bcffff0
Roshan Adhikari <nahsorad@gmail.com>
2022-12-05 04:27:31 +0545

    Register Eureka clients

--- inventory-service/src/main/java/np/roshanadh/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/np/roshanadh/inventoryservice/InventoryServiceApplication.java
@@ -6,10 +6,12 @@ import np.roshanadh.inventoryservice.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @Slf4j
+@EnableEurekaClient
 public class InventoryServiceApplication {
 
 	@Bean
