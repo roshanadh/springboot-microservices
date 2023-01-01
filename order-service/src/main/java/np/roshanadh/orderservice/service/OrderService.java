244613ee6de7368c5f72e2679acdd6aabe806491
Roshan Adhikari <nahsorad@gmail.com>
2023-01-01 22:39:35 +0545

    Return success message from service layer

--- order-service/src/main/java/np/roshanadh/orderservice/service/OrderService.java
+++ order-service/src/main/java/np/roshanadh/orderservice/service/OrderService.java
@@ -23,7 +23,7 @@ public class OrderService {
   private final OrderRepository orderRepository;
   private final WebClient.Builder webClientBuilder;
 
-  public void placeOrder(OrderRequest orderRequest) {
+  public String placeOrder(OrderRequest orderRequest) {
     Order order = new Order();
     order.setOrderNumber(UUID.randomUUID().toString());
 
@@ -52,7 +52,10 @@ public class OrderService {
     boolean allProductsInStock = Arrays.stream(inventoryResponses)
             .allMatch(InventoryResponse::isInStock);
 
-    if (allProductsInStock) orderRepository.save(order);
+    if (allProductsInStock) {
+      orderRepository.save(order);
+      return "Order placed successfully!";
+    }
     else throw new IllegalArgumentException("Order out of stock");
   }
 
