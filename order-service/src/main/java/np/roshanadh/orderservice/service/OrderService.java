80dc7706b4eb4b1c652fe7638de437e06ac4c4ca
Roshan Adhikari <nahsorad@gmail.com>
2023-01-01 23:29:04 +0545

    Create custom Span for placeOrder logic

--- order-service/src/main/java/np/roshanadh/orderservice/service/OrderService.java
+++ order-service/src/main/java/np/roshanadh/orderservice/service/OrderService.java
@@ -1,12 +1,15 @@
 package np.roshanadh.orderservice.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import np.roshanadh.orderservice.dto.InventoryResponse;
 import np.roshanadh.orderservice.dto.OrderLineItemsDto;
 import np.roshanadh.orderservice.dto.OrderRequest;
 import np.roshanadh.orderservice.model.Order;
 import np.roshanadh.orderservice.model.OrderLineItems;
 import np.roshanadh.orderservice.repository.OrderRepository;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -18,10 +21,12 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class OrderService {
 
   private final OrderRepository orderRepository;
   private final WebClient.Builder webClientBuilder;
+  private final Tracer tracer;
 
   public String placeOrder(OrderRequest orderRequest) {
     Order order = new Order();
@@ -39,24 +44,36 @@ public class OrderService {
             .map(OrderLineItems::getSkuCode)
             .collect(Collectors.toList());
 
-    // Call Inventory Service, and place order if product is in stock
-    String inventoryServiceUri = "http://inventory-service/api/inventory";
-    InventoryResponse[] inventoryResponses = webClientBuilder.build()
-            .get()
-            .uri(inventoryServiceUri,
-                    uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
-            .retrieve()
-            .bodyToMono(InventoryResponse[].class)
-            .block();
+    log.info("Calling inventory service");
+    // create a Span ID
+    Span inventoryServiceLookup = tracer
+            .nextSpan()
+            // give a unique name to this particular span
+            .name("inventoryServiceLookup");
+    // execute the placeOrder logic under the just-created span ID
+    try (Tracer.SpanInScope spanInScope = tracer.withSpan(inventoryServiceLookup.start())) {
+      // Call Inventory Service, and place order if product is in stock
+      String inventoryServiceUri = "http://inventory-service/api/inventory";
+      InventoryResponse[] inventoryResponses = webClientBuilder.build()
+              .get()
+              .uri(inventoryServiceUri,
+                      uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+              .retrieve()
+              .bodyToMono(InventoryResponse[].class)
+              .block();
 
-    boolean allProductsInStock = Arrays.stream(inventoryResponses)
-            .allMatch(InventoryResponse::isInStock);
+      boolean allProductsInStock = Arrays.stream(inventoryResponses)
+              .allMatch(InventoryResponse::isInStock);
 
-    if (allProductsInStock) {
-      orderRepository.save(order);
-      return "Order placed successfully!";
+      if (allProductsInStock) {
+        orderRepository.save(order);
+        return "Order placed successfully!";
+      } else throw new IllegalArgumentException("Order out of stock");
+    } finally {
+      inventoryServiceLookup.end();
     }
-    else throw new IllegalArgumentException("Order out of stock");
+
+
   }
 
   private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
